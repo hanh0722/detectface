9d35f76dc676714505f6f3df43b4e44d0d892dd8
hanh0722 <78199926+hanh0722@users.noreply.github.com>
2021-05-15 10:17:45 +0700

    Updates

---
+++ precache-manifest.8ed1ea0f789a903be619c0a48d2e8100.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "30b4e953b3c6cbd8d750",
+    "url": "/detectface/static/css/main.e84f3587.chunk.css"
+  },
+  {
+    "revision": "30b4e953b3c6cbd8d750",
+    "url": "/detectface/static/js/main.bcb07096.chunk.js"
+  },
+  {
+    "revision": "22aee5eb8a4d91074034",
+    "url": "/detectface/static/js/runtime~main.ed188198.js"
+  },
+  {
+    "revision": "5c35c8db2053752dd0d2",
+    "url": "/detectface/static/css/2.d0eb269f.chunk.css"
+  },
+  {
+    "revision": "5c35c8db2053752dd0d2",
+    "url": "/detectface/static/js/2.0a12eeca.chunk.js"
+  },
+  {
+    "revision": "f0095bb47309936ebbd9",
+    "url": "/detectface/static/js/3.83c1415e.chunk.js"
+  },
+  {
+    "revision": "05d726ec4437099d797ba3d6f69411f5",
+    "url": "/detectface/index.html"
+  }
+];
